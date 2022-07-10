93376afaaf351d50de2a976423b0adb03cd6c3ab
Ben Linkous <benlinkous@gmail.com>
2022-07-10 09:21:49 -0400

    THe video says to do it

---
+++ Git link.R
@@ -0,0 +1,10 @@
+print(this file was created within RStudio.Version
+      
+      print ("and now it lives on Github")
+      
+      
+      
+print("this file was created within Rstudio") 
+print("and now it lives on Github")
+
+      
